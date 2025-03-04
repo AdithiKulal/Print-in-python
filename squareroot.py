a774be8ec268866a362ac63c57c70b1139868f72
AdithiKulal <kulalamithadinakar@gmail.com>
2025-03-04 19:08:34 +0530

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a number: "))
+if num<0:
+    print("Error: Square root of negative number is not a real number.")
+else:
+    sqrt=num**0.5
+    print("The square root of",num," is ",sqrt)
