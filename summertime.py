1bd8d9ed839f49f7ebede84d7d49f1bc80d4c418
AdithiKulal <kulalamithadinakar@gmail.com>
2025-03-04 17:29:17 +0530

    Add files via upload

---
+++ summertime.py
@@ -0,0 +1,7 @@
+temp=float(input("Enter the temperature(in Celsius): "))
+if temp<15:
+    print("It's too cold! Wear warm clothes")
+elif temp<25:
+    print("It's a good day to wear light clothes!")
+else:
+    print("It's too hot! Stay hydrated and wear light clothes, breathable clothes.")
