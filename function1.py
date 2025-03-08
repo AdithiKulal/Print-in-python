a1e380cacd80840477c5b6e50e57321c8fc8360c
AdithiKulal <kulalamithadinakar@gmail.com>
2025-03-08 19:56:45 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,9 @@
+def coding():
+    print("Coding is the process of writing instructions for computers to perform tasks. It's also known as computer programming.")
+    print("Coding is used to create instructions for computers to perform tasks, such as building websites and apps, processing data, and automating tasks.")
+coding()
+
+def nature():
+    print("Nature is the physical world around us, including all living things and the environment.")
+    print("It is the source of all life on Earth, and it provides us with food, water, air, and other resources.")
+nature()
