4809626b28680a69bce0bbd46939106f39445bc0
AdithiKulal <kulalamithadinakar@gmail.com>
2025-03-30 10:05:38 +0530

    Add files via upload

---
+++ mathematicaloperators.py
@@ -0,0 +1,12 @@
+import math
+
+x = float(input("Enter a value for x: "))
+y = float(input("Enter a value for y: "))
+
+print("Ceil of x:", math.ceil(x))
+print("Copy sign of y to x:", math.copysign(x, y))
+print("Factorial of x :", math.factorial(int(x)))
+print("Floor of x:", math.floor(x))
+print("Is x NaN?", math.isnan(x))
+print("Expnential of x:", math.exp(x))
+print("Degrees of x:", math.degrees(x))
