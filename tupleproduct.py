dba5935f3114b1d8d5a7221503f7729cc81f2b41
AdithiKulal <kulalamithadinakar@gmail.com>
2025-04-07 17:08:16 +0530

    Add files via upload

---
+++ tupleproduct.py
@@ -0,0 +1,9 @@
+def calculate_product(numbers):
+    product=1
+    for num in numbers:
+        product*=num
+    return product
+
+numbers=(2,3,4,5)
+result= calculate_product(numbers)
+print("The product of the numbers is:",result)
