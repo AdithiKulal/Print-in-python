a0441f28aebd3ea830e71425e8e80b482dcb1489
AdithiKulal <kulalamithadinakar@gmail.com>
2025-04-11 19:29:32 +0530

    Add files via upload

---
+++ dictionary2.py
@@ -0,0 +1,23 @@
+student_data = {'id1':
+        {'name':['Sara'],
+        'class':['v'],
+        'subject_integration':['english, maths, science']},
+        'id2':
+        {'name': ['David'],
+        'class': ['v'],
+        'subject_integration': ['english, maths, science']},
+        'id3':
+        {'name': ['Sara'],
+        'class': ['v'],
+        'subject_integration': ['english, maths, science']},
+        'id4':
+        {'name': ['Mehtej'],
+        'class': ['v'],
+        'subject_integration': ['english, maths, science']
+},}
+
+print('no. 1', student_data["id1"])
+print('no. 2', student_data["id2"])
+
+x = student_data.values()
+print(x)
