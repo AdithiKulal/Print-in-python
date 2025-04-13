919cc54a973b8ab6d9710c08ee3ee26713de8fb4
AdithiKulal <kulalamithadinakar@gmail.com>
2025-04-13 09:34:32 +0530

    Add files via upload

---
+++ symset.py
@@ -0,0 +1,7 @@
+set1 = {"apple", "banana", "cherry", "mango", "kiwi", "pineapple"}
+set2 = {"banana", "orange", "grape", "watermelon", "strawberry", "blueberry"}
+
+symmetric_difference = set1.symmetric_difference(set2)
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Symmetric difference:", symmetric_difference)
