005078345e0df7794a0a3e28a9790a920d4a8f3f
AdithiKulal <kulalamithadinakar@gmail.com>
2026-01-16 19:56:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-a= int(input("enter a number:"))
-if a>0:
-    print("Its a positive number")
-elif a==0:
-    print("Its 0!")
-else:
-    print("Its a negative number")
+from flask import Flask, request, render_template
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+
+def calculate_age():
+    try:
+        birth_year = int(request.form.get('birth_year'))
+        current_year = datetime.now().year
+
+        if birth_year > current_year or birth_year < 1900:
+            return render_template('index.html', error="Please enter a valid year between 1900 and the current year.")
+        
+        age = current_year - birth_year
+
+        return render_template("index.html", age=age)
+    except ValueError:
+        return render_template("index.html", error="Please enter a valid year.")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
