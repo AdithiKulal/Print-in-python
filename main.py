e95def4bb9c831219a8330c8b5bc54470bf68ac7
AdithiKulal <kulalamithadinakar@gmail.com>
2026-01-23 19:51:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-from flask import Flask, request, render_template
-from datetime import datetime
+from flask import Flask, render_template
+import json
+import urllib.request
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
+def details():
+    location = input("Enter the location here: ")
+    api_key = 'AQw4sNOX1A9KQf6L5TTn_2Ggsrgc63hY04_h8ps'
 
-def home():
+    try:
+        source = urllib.request.urlopen(
+            'https://geocode.search.hereapi.com/v1/geocode?apikey=' + api_key + '&q=' + location
+        ).read()
+
+        responseData = json.loads(source)
+
+        data = {
+            "latitude": str(responseData['items'][0]['position']['lat']),
+            "longitude": str(responseData['items'][0]['position']['lng']),
+        }
+        return render_template('index.html', data=data, apikey=api_key)
+    except (Exception):
+        return render_template('index.html', error="Give the correct location")
+
+@app.route('/')
+def index():
     return render_template('index.html')
 
-@app.route('/calculate', methods=['POST'])
-
-def calculate_age():
-    try:
-        birth_year = int(request.form.get('birth_year'))
-        current_year = datetime.now().year
-
-        if birth_year > current_year or birth_year < 1900:
-            return render_template('index.html', error="Please enter a valid year between 1900 and the current year.")
-        
-        age = current_year - birth_year
-
-        return render_template("index.html", age=age)
-    except ValueError:
-        return render_template("index.html", error="Please enter a valid year.")
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+app.run(host='0.0.0.0', port=8080)
